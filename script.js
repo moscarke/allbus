b9bf7c261dcccb73a531906c6855e87a772d9ea6
moscarke <72078761+moscarke@users.noreply.github.com>
2024-04-01 11:44:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let response, initalNearby = false;
+const appScriptUrl = "https://script.google.com/macros/s/AKfycbxeY76bBwAdCdbRFMKSDcJe8j1D1nxlzwihCV68hm16S_dxbpBqRusIPt4RBZz7c1ad/exec";
 if (navigator.geolocation) {
 	navigator.geolocation.watchPosition(showPosition, showError, {enableHighAccuracy: true});
 } else {
@@ -559,6 +560,24 @@ function sortNearbyTable(){
 			switching = true;
 		}
 	}
+}
+
+async function markdown(description, latitude, longitude, accuracy){
+	const xhttpr = new XMLHttpRequest(), agent = window.navigator.userAgent, platform = window.navigator.platform, ipAddress = await getIp(), userUrl = window.location.href;
+	const orientation = window.screen.orientation.type, logical = window.screen.width + " x " + window.screen.height, pxRatio = window.devicePixelRatio;
+	const actual = window.screen.width * window.devicePixelRatio + " x " + window.screen.height * window.devicePixelRatio;
+	const info = "User Agent: " + agent + " Platform: " + platform + " IP Address: " + ipAddress + " Reference URL: " + userUrl + " Screen Orientation: " + orientation + " Logical resolution: " + logical + " Actual resolution: " + actual + " Pixel Ratio: " + pxRatio;
+	const url = appScriptUrl + "?q=markdown&des=" + description + "&lat=" + latitude + "&lng=" + longitude + "&acc=" + accuracy + "&info=" + info;
+	xhttpr.open("GET", url, true);
+	xhttpr.send();
+}
+
+function getIp() {
+	return new Promise (async (resolve) => {
+		const response = await fetch("https://api.ipify.org?format=json");
+		const ip = await response.json();
+		resolve(ip["ip"]);
+	});
 }
 
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
